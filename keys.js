eca52c4de02c2305f3ce486d60dfb846a21101f3
Deepraj Dey <ddeepraj47@gmail.com>
2022-11-11 21:01:16 +0530

    updated

--- keys.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "AIzaSyCp4zPZH_Dd8a1Wewp8BUK22i7w1XMYi-A";
-export const CONTEXT_KEY = "358909a98ba584404";
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Header from "./../components/Header";
 import React from "react";
-import { API_KEY, CONTEXT_KEY } from "../keys";
 import Response from "../Response";
 import SearchResults from "../components/SearchResults";
 
@@ -31,7 +30,7 @@ export async function getServerSideProps(context) {
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
       ).then((response) => response.json());
   return {
     props: {
